5afd46fcd9e6235502092bb687d2bd5ac6b3b0a1
Theresa Eliger <910theresa@gmail.com>
2018-05-02 17:21:28 -0700

    Python - Flask Fundamentals Assignments

---
+++ playground/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'This is the index page'
+@app.route('/play/<x>')
+def more_boxes(x):
+    return render_template('index.html', num=int(x))
+@app.route('/play/<x>/<color>')
+def green_boxes(x, color):
+    return render_template('index.html', num=int(x), color=str(color))
+
+app.run(debug=True)
