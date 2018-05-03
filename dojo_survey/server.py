7c0225e4847a907122039a88987e0481145977c2
Theresa Eliger <910theresa@gmail.com>
2018-05-03 16:06:34 -0700

    Added: User will be redirected when trying to access /danger.

--- dojo_survey/server.py
+++ dojo_survey/server.py
@@ -10,5 +10,10 @@ def survey_results():
     name = request.form['name']
     return render_template('result.html')
 
+@app.route('/danger')
+def danger():
+    print('\n\n\nA user tried to visit /danger. They are being redirected back to /.')
+    return redirect('/')
+
 if __name__=="__main__":
     app.run(debug=True)
